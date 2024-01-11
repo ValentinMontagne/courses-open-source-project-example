5118f93955eae4a945d7b5a7888dc32c20fa4a88
Valentin MONTAGNE <vm@marvelab.co>
2024-01-11 14:13:48 +0100

    chore: demo

--- src/counter.ts
+++ src/counter.ts
@@ -3,6 +3,7 @@ export function count(state: number): number {
 }
 
 export function setupCounter(element: HTMLButtonElement): void {
+  var toto;
   let counter = 0;
 
   const setCounter = (count: number): void => {
--- test/counter.test.ts
+++ test/counter.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest';
 import { count } from '../src/counter';
 
 test('count with state at 3 should be equal to 4', () => {
-  expect(count(3)).toBe(4);
+  expect(count(3)).toBe(5);
 });
